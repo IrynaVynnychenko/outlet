bebb539f8e53a50c43383468c802ec6381c9731e
IrynaVynnychenko <i.vynnychenko@gmail.com>
2019-07-04 23:41:24 +0300

    push homework

--- main.js
+++ main.js
@@ -1,52 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
-    function Shop(category, goods, amount) {
-        this.department = 6;
-        this.owners = 2;
-        this.basket = 1;
+    function Shop(shops, category, amount) {
         this.category = category;
-        this.goods = goods;
-        this.specialize = function() {
-            return this.category + ' ' + this.goods
-        };
+        this.amount = amount;
+        this.shops = shops;
 
-        let _amount = [amount];
-        this.getAmount = function() {
-            console.log(_amount);
-        };
-        this.setAmount = function(a) {
-            _amount.push(a);
+        let sum = document.querySelector('input');
+        let order = document.querySelector('.order');
+        let buttonPlus = document.querySelector('.plus');
+        let buttonMinus = document.querySelector('.minus');
+        let dropdown = document.querySelector('select');
+        let ourShops = document.querySelector('thead tr');
+        let left = document.querySelector('tbody tr');
+        let self = this;
+
+        for (let i = 0; i < shops.length; i++) {
+            let a = document.createElement('td');
+            a.innerHTML = shops[i];
+            ourShops.appendChild(a);
         }
 
-        const self = this;
+        for (let i = 0; i < shops.length; i++) {
+            let b = document.createElement('td');
+            b.innerHTML = amount[i];
+            left.appendChild(b);
+        }
 
-        this.obj = {
-            a: 7,
-            b: function() {
-                console.log(self.goods);
+        for (let i = 0; i < shops.length; i++) {
+            let c = document.createElement('option');
+            c.innerHTML = category[i];
+            dropdown.appendChild(c);
+        }
+
+        buttonPlus.addEventListener('click', function() {
+            let shopBalance = document.querySelectorAll('tbody td')
+            for (let i = 0; i < category.length; i++) {
+                if (dropdown.value === category[i]) {
+                    amount[i] = +sum.value + +amount[i];
+                    if (amount[i] < 10) {
+                        alert('Срочно заказывать товар');
+                        order.innerHTML = 10 - +amount[i];
+                        shopBalance[i].innerHTML = +shopBalance[i].innerHTML + +sum.value;
+                    } else if (amount[i] >= 10) {
+                        order.innerHTML = '';
+                        alert('Не торопись заказывать товар');
+                        shopBalance[i].innerHTML = +shopBalance[i].innerHTML + +sum.value;
+                    }
+
+
+                }
             }
-        }
+
+        });
+
+        buttonMinus.addEventListener('click', function() {
+            let shopBalance = document.querySelectorAll('tbody td')
+            for (let i = 0; i < category.length; i++) {
+                if (dropdown.value === category[i]) {
+                    amount[i] = +amount[i] - +sum.value;
+                    if (amount[i] < 10) {
+                        alert('Срочно заказывать товар');
+                        order.innerHTML = 10 - +amount[i];
+                        shopBalance[i].innerHTML = +shopBalance[i].innerHTML - +sum.value;
+                    } else if (amount[i] >= 10) {
+                        order.innerHTML = '';
+                        alert('Не торопись заказывать товар');
+                        shopBalance[i].innerHTML = +shopBalance[i].innerHTML - +sum.value;
+                    }
+                }
+            }
+
+        });
     }
-    let paragraph1 = document.querySelector('.first');
-    let silpo = new Shop('products', 'milk', 10); // создаем новый экземпляр конструктора person, можем со скобочками, можем без ()
+    let silpo = new Shop(['Silpo', 'Kosmo', 'Foxtrot'], ['products', 'chemical', 'technikals'], [6, 3, 5]); // создаем новый экземпляр конструктора person, можем со скобочками, можем без ()
     console.log(silpo);
-    let chuckcha = silpo.specialize();
-    console.log(chuckcha);
-    silpo.obj.b();
-    silpo.setAmount(5);
-    silpo.getAmount();
-
-    paragraph1.innerHTML = silpo.specialize();
-
-    let paragraph = document.querySelector('.second');
-    let foxtrot = new Shop('technique', 'blenders', 5);
-    console.log(foxtrot);
-
-    paragraph.innerHTML = foxtrot.specialize();
-
-    foxtrot.obj.b();
-    foxtrot.setAmount(15);
-    foxtrot.getAmount();
 });
 
 // гетеры - читают 
 // сетеры - меняют
+
+//e.preventDefault(); - пишем в форме, что бы небыло перезагрузки страницы при заполнении формы
